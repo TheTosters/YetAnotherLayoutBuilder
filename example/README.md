17db0faf6e5290caa9156dd964ff71624ed56ded
Bartłomiej Żarnowski <thetosters@gmail.com>
2021-11-30 10:23:44 +0100

    Initial project commit.
    Signed-off-by: Bartłomiej Żarnowski <thetosters@gmail.com>

---
+++ example/README.md
@@ -0,0 +1,7 @@
+# Example
+
+This is example to Yet Another Layout Builder library.
+
+## More info
+
+Later... probably...
---
+++ lib/yet_anoter_layout_builder.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library yet_anoter_layout_builder;
+
+export 'src/yet_anoter_layout_builder_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
